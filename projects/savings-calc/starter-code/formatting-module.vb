7075edb9d751dc3d884193bdacd98d0e7f0bb23d
MJ Rossetti <datacreativellc@gmail.com>
2018-11-08 00:29:55 -0500

    Link to future URLs

--- projects/savings-calc/starter-code/formatting-module.vb
+++ projects/savings-calc/starter-code/formatting-module.vb
@@ -2,7 +2,7 @@
 ' SAVINGS CALCULATOR PROJECT > STARTER CODE > FORMATTING MODULE
 '
 ' Author: Prof. Rossetti <prof.mj.rossetti@gmail.com>
-' Source: _________________
+' Source: https://github.com/prof-rossetti/georgetown-opim-557-201810/blob/master/projects/savings-calc/starter-code/formatting-module.vb
 ' License: Students, feel free but not obligated to use this module in your project as long as you retain this attribution section. If you wrote something like this on your own, no need to attribute. If this code inspired you to write your own code, please still consider providing an attribution link to this file's GitHub URL.
 '
 
